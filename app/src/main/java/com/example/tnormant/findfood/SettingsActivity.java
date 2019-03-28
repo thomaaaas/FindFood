9245c8400042e63554a2be04c74c6c36e2a421e3
thomaaaas <thomasbfjfn@gmail.com>
2019-03-29 00:54:55 +0100

    Settings Fix

--- app/src/main/java/com/example/tnormant/findfood/SettingsActivity.java
+++ app/src/main/java/com/example/tnormant/findfood/SettingsActivity.java
@@ -11,7 +11,7 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportFragmentManager().beginTransaction()
-                .replace(R.id.content, new SettingsFragment())
+                .replace(android.R.id.content, new SettingsFragment())
                 .commit();
     }
 }
